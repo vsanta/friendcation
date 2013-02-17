1a688898f96ce9183cd7d8cf57177d8b3720eb04
vsanta <vanessa.santanna@gmail.com>
2013-02-17 13:38:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crashhere::Application.config.secret_token = '673dab74a9dad42c4c1a3066f52c790890e91b613c5bde640378a246818aac1094f9293f71edaa22393c74fd4bcf750eb009a5f0a14ec203b1e8284357cae423'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Crashhere::Application.config.session_store :cookie_store, key: '_crashhere_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Crashhere::Application.config.session_store :active_record_store
